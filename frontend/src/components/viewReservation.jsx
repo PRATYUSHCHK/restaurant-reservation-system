2182464ac51d3c1ab0b0ed4bb42686ec0d705666
Campus Care <pratyushchk2024@gmail.com>
2026-05-31 21:54:44 +0530

    Fix CSS filename case mismatch

--- frontend/src/components/viewReservation.jsx
+++ frontend/src/components/viewReservation.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "./ViewReservation.css";
+import "./viewReservation.css";
 import toast from "react-hot-toast";
 
 const ViewReservation = () => {
